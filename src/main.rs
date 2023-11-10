e9b7d82e1d0ecfdd5512c825f45ab3fb3da813ad
yusongbin <yusongbin@meituan.com>
2023-11-10 19:15:19 +0800

    feat: 任意类型排序

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-pub fn bubble_sort<T: Ord>(arrays: &mut[T]) {
+pub fn bubble_sort<T: PartialOrd>(arrays: &mut[T]) {
     let len = arrays.len();
     for i in 0..len {
         for j in 0..(len - i - 1) {
@@ -10,8 +10,11 @@ pub fn bubble_sort<T: Ord>(arrays: &mut[T]) {
 }
 
 fn main() {
-    let mut arr = [28, 47, 6, 75, 34, 63, 82, 11];
+    let mut arr = [28.1, 47.1, 6.6, 75.7, 34.3, 6.3, -82.9, -11.1];
     bubble_sort(&mut arr);
-    println!("{:?}", arr);  // 输出：[1, 2, 3, 4, 5, 6, 7, 8]
+    println!("{:?}", arr);  // 输出：[-82.9, -11.1, 6.3, 6.6, 28.1, 34.3, 47.1, 75.7]
+    let mut words = ["rust", "is", "the", "best", "language"];
+    bubble_sort(&mut words);
+    println!("{:?}", words); // 输出：["best", "is", "language", "rust", "the"]
 }
 
